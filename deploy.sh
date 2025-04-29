ad1538e78017095e25aaf0225a4b7c0ddb3a1bb0
Brian Louie Cubero <blouiecubero@google.com>
2025-04-29 13:20:20 +0800

    added readme

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Configuration
-PROJECT_ID="cubz-sample-project-gcp"
-CLUSTER_NAME="autopilot-cluster-1"
-CLUSTER_ZONE="asia-southeast1-a"
+export PROJECT_ID="cubz-sample-project-gcp"
+export CLUSTER_NAME="autopilot-cluster-1"
+export CLUSTER_REGION="asia-southeast1"
 
 # Build the Docker image using Cloud Build
 echo "Building Docker image..."
-gcloud builds submit --tag gcr.io/${PROJECT_ID}/hello-world:latest .
+gcloud builds submit --tag gcr.io/${PROJECT_ID}/hi-world:latest .
 
 # Get GKE credentials
 echo "Getting GKE credentials..."
-gcloud container clusters get-credentials ${CLUSTER_NAME} --zone ${CLUSTER_ZONE} --project ${PROJECT_ID}
+gcloud container clusters get-credentials ${CLUSTER_NAME} --region ${CLUSTER_REGION} --project ${PROJECT_ID}
 
 # Deploy to GKE
 echo "Deploying to GKE..."
@@ -19,8 +19,8 @@ kubectl apply -f deployment.yaml
 
 # Wait for deployment to be ready
 echo "Waiting for deployment to be ready..."
-kubectl rollout status deployment/hello-world
+kubectl rollout status deployment/hi-world
 
 # Get the external IP
 echo "Getting external IP..."
-kubectl get service hello-world 
+kubectl get service hi-world 
